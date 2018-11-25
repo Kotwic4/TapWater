49525368898aa5b4b7b97a1f734472d8bd3a6ce2
Radomir Krawczykiewicz <radomir.krawczykiewicz@swmansion.com>
2018-11-25 09:22:51 +0100

    change initial data

--- src/main/java/pl/tapwater/LoadWaterConditions.java
+++ src/main/java/pl/tapwater/LoadWaterConditions.java
@@ -13,7 +13,7 @@ public class LoadWaterConditions {
         return args -> {
             log.info("Preloading" + waterConditionRepository.save(new WaterCondition("Gdańsk", 0, 0, 350, 190, 40, 0.4f, 30, false)));
             log.info("Preloading" + waterConditionRepository.save(new WaterCondition("Warszawa", 4, 5, 350, 500, 60, 0.55f, 100, false)));
-            log.info("Preloading" + waterConditionRepository.save(new WaterCondition("Kraków", 1, 1, 350, 190, 55, 0.4f, 60, false)));
+            log.info("Preloading" + waterConditionRepository.save(new WaterCondition("Kraków", 1, 1, 450, 190, 55, 0.4f, 40, false)));
         };
     }
 }
