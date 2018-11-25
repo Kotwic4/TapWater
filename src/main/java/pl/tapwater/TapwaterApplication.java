4335624b6011fbc47b611c6b0910faade555458f
Radomir Krawczykiewicz <radomir.krawczykiewicz@swmansion.com>
2018-11-25 06:03:08 +0100

    initial commit

---
+++ src/main/java/pl/tapwater/TapwaterApplication.java
@@ -0,0 +1,12 @@
+package pl.tapwater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapwaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapwaterApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/tapwater/TapwaterApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.tapwater;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TapwaterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
